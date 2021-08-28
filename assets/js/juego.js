a583272dccb08452a1761cfd1be0a256bed10fcc
Tu nombre <arafael.menchaca@gmail.com>
2021-08-28 04:29:09 -0500

    Se termina la logica del juego y la funcion de los botones (ya es jugable)

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -9,9 +9,21 @@
     const tipos      = ['C', 'D', 'H', 'S'];
     const especiales = ['A', 'J', 'Q', 'K'];
 
+    let puntosJugador = 0,
+        puntosComputadora = 0;
 
+    // Referencias del HTML
+    const btnPedir   = document.querySelector('#btnPedir');
+    const btnDetener = document.querySelector('#btnDetener');
+    const btnNuevo   = document.querySelector('#btnNuevo');
+
+    const divCartasJugador      = document.querySelector('#jugador-cartas');
+    const divCartasComputadora  = document.querySelector('#computadora-cartas');
+    const puntosHTML = document.querySelectorAll('small');
+
+    
     // Esta funcion crea un nuevo deck
-    const creadDeck = () => {
+    const crearDeck = () => {
 
         for (let i = 2; i <= 10; i++){
             for( let tipo of tipos){
@@ -30,7 +42,7 @@
         return deck;
     }
 
-    creadDeck();
+    crearDeck();
 
 
     
@@ -40,46 +52,12 @@
         if ( deck.length === 0){
             throw 'No hay cartas en el deck';
         }
-
         const carta = deck.pop();
-
-        console.log(deck);
-        console.log(carta); // carta debe ser de la braja y dejar de existur
         return carta;
     }
     
-    // pedirCarta();
-
-    // const valorCarta = (carta) => {
-
-    //     const valor = carta.substring(0, carta.length -1);
-    //     let puntos = 0;
-    //     // 2 = 2  10 = 10  3 = 3
-    //     if ( isNaN(valor)){
-
-    //         puntos = ( valor === 'A') ? 11 : 10;
-            
-    //     } else {
-            
-    //         puntos = valor * 1;
-    //     }
-        
-    // }
-
-    // valorCarta('9D');
-
-    // const valorCarta = (carta) => {
-
-    //     const valor = carta.substring(0, carta.length -1);
-    //     let puntos = 0;
-    //     // 2 = 2  10 = 10  3 = 3
-    //     puntos = ( isNaN(valor) === 'A') ? 11 :
-    //              ( isNaN(valor) === 'A') ? 10 : valor * 1;        
-    // }
-
-    // valorCarta('9D');
-
-        const valorCarta = (carta) => {
+        // pedirCarta();
+    const valorCarta = (carta) => {
 
         const valor = carta.substring(0, carta.length -1);
         return ( isNaN( valor ) ) ?
@@ -87,5 +65,89 @@
                 : valor * 1;      
     }
 
-    const valor = valorCarta(pedirCarta());
-    console.log({valor});
+    // turno de la computadora
+    const turnoComputadora = (puntosMinimos) => {
+
+        do {
+        const carta = pedirCarta();
+        
+        puntosComputadora = puntosComputadora + valorCarta(carta);
+        puntosHTML[1].innerText = puntosComputadora;
+
+        const imgCarta = document.createElement('img');
+        imgCarta.src = `assets/cartas/${ carta }.png`;
+        imgCarta.classList.add('carta');        
+        divCartasComputadora.append( imgCarta );
+
+        if ( puntosMinimos > 21){
+            break;
+        }
+
+        } while( (puntosComputadora < puntosMinimos) && ( puntosMinimos <= 21) );
+
+
+        setTimeout(() => {
+            if (puntosComputadora === puntosMinimos){
+                alert('Nadie Gana');
+            } else if ( puntosMinimos > 21 ){
+                alert('Computadora Gana');
+            } else if (puntosComputadora > 21) {
+                alert('jugador Gana');
+            } else {
+                alert('Computadora Gana');
+            }
+        }, 100);
+    }
+
+    
+    // Eventos
+    btnPedir.addEventListener('click', () => {
+        
+        const carta = pedirCarta();
+        
+        puntosJugador = puntosJugador + valorCarta(carta);
+        puntosHTML[0].innerText = puntosJugador;
+
+        const imgCarta = document.createElement('img');
+        imgCarta.src = `assets/cartas/${ carta }.png`;
+        imgCarta.classList.add('carta');        
+        divCartasJugador.append( imgCarta );
+
+        if ( puntosJugador > 21 ){
+            console.warn('Lo siento mucho perdiste');
+            btnPedir.disabled = true;
+            btnDetener.disabled = true;
+            turnoComputadora(puntosJugador);
+
+        } else if ( puntosJugador === 21){
+            console.warn('21, genial');
+            btnPedir.disabled = true;
+            btnDetener.disabled = true;
+            turnoComputadora(puntosJugador);
+        }
+    });
+
+    btnDetener.addEventListener('click', () => {
+        btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputadora(puntosJugador);
+    });
+
+    btnNuevo.addEventListener('click', () => {
+
+        console.clear(); 
+        deck = [];
+        deck = crearDeck();
+
+        puntosJugador     = 0;
+        puntosComputadora = 0;
+
+        puntosHTML[0].innerText = 0;
+        puntosHTML[1].innerText = 0; 
+
+        divCartasComputadora.innerHTML = '';
+        divCartasJugador.innerHTML     = '';
+
+        btnPedir.disabled = false;
+        btnDetener.disabled = false;           
+      });
